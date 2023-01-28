9e19529cfc4f38e45045ed65b4498c7544baf9a0
Paul Bodner <bodner80@gmail.com>
2023-01-28 13:28:46 -0800

    refactored mcbutton animation

--- aot-next-front/components/global/Mcbutton.jsx
+++ aot-next-front/components/global/Mcbutton.jsx
@@ -1,35 +1,36 @@
-import {motion, AnimatePresence} from 'framer-motion'
-import { mcbuttonTop, mcbuttonMiddle, mcbuttonBottom } from '../../utils/motion/motionObjects'
+import {motion, AnimatePresence, MotionConfig} from 'framer-motion'
+import { top, middle, bottom } from '../../utils/motion/motionObjects'
 
 export default function Mcbutton({ styles, menuOpen, toggleMenuOpen }){
-
 
   return(
     <div className={ styles.mcbuttonContainer } onClick={ () => { toggleMenuOpen() } }>
       <AnimatePresence>
-        { !menuOpen && <motion.div 
-          className={ styles.mcbutton }
-          key="mcButtonClosed"
-          initial="initial"
-          animante="animate"
-          exit="exit"
-          >
-          <motion.b variants={ mcbuttonTop.openToClosed }></motion.b>
-          <motion.b variants={ mcbuttonMiddle.openToClosed }></motion.b>
-          <motion.b variants={ mcbuttonBottom.openToClosed }></motion.b>
-        </motion.div> }
+        <MotionConfig transition={{ duration: .5, type: "spring", stiffness: 200  }}>
+          { menuOpen && <motion.div 
+            className={ styles.mcbutton }
+            key="mcButtonClosed"
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            >
+            <motion.b variants={ top.openToClosed }></motion.b>
+            <motion.b variants={ middle.openToClosed }></motion.b>
+            <motion.b variants={ bottom.openToClosed }></motion.b>
+          </motion.div> }
 
-        { menuOpen && <motion.div 
-          className={ styles.mcbutton }
-          key="mcButtonOpen"
-          initial="initial"
-          animante="animate"
-          exit="exit"
-        >
-          <motion.b variants={ mcbuttonTop.closedToOpen }></motion.b>
-          <motion.b variants={ mcbuttonMiddle.closedToOpen }></motion.b>
-          <motion.b variants={ mcbuttonBottom.closedToOpen }></motion.b>
-        </motion.div> }
+          { !menuOpen && <motion.div 
+            className={ styles.mcbutton }
+            key="mcButtonOpen"
+            initial="initial"
+            animate="animate"
+            exit="exit"
+          >
+            <motion.b variants={ top.closedToOpen }></motion.b>
+            <motion.b variants={ middle.closedToOpen }></motion.b>
+            <motion.b variants={ bottom.closedToOpen }></motion.b>
+          </motion.div> }
+        </MotionConfig>
       </AnimatePresence>
     </div>
   );
